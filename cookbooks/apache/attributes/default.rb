fa0e248158f8f9d2932a1efc308fe2460343c3e2
U-DIR\jaime.gaona <jaime.gaona@MW7TW92ZRHVXSC.dir.svc.accenture.com>
2016-02-09 11:42:53 -0600

    added user recipe and data bag

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -4,3 +4,5 @@
 # Set a default name
 
 default["apache"]["indexfile"] = "index1.html"
+default["apache"]["sites"]["clowns"] = { "port" => 80 }
+default["apache"]["sites"]["bears"] = { "port" => 81 } 
